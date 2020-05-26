476620e268c0e639b19248e4bbb93a75eab02f29
daryn2 <amantaevdaryn@gmail.com>
2020-05-26 15:00:04 +0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:8080'
+            apiUrl: 'http://soulai.tech'
         })
     }
 }
